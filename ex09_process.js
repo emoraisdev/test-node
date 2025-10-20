d08397945da8c617c008195e6148738f56ca2f1b
emoraisdev <evertonsmorais@gmail.com>
2025-10-20 20:51:24 -0300

    feat: Criando novos exemplos

---
+++ ex09_process.js
@@ -0,0 +1,8 @@
+process.stdout.write('Está gostando do curso?')
+
+process.stdin.on('data', function(data) {
+
+    process.stdout.write(`Sua resposta foi ${data.toString()}Obrigado!\n`)
+    process.exit()
+
+})
---
+++ ex11_http.js
@@ -0,0 +1,12 @@
+const http = require('http')
+
+const server = http.createServer(function(req,res) {
+
+    res.writeHead(200, {"Content-type": "text/html"})
+    res.end('<h1>Acho que é melhor usar Express, não?</h1>')
+})
+
+const porta = 3456
+server.listen(porta, function(){
+    console.log(`Escutando a porta: ${porta}`)
+})
